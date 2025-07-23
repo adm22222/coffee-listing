0386129a17b420283500e3361a256a971d687a8b
adm22222 <166756455+adm22222@users.noreply.github.com>
2025-07-23 14:52:05 +0300

    Add files via upload

--- components/ProductPanel.tsx
+++ components/ProductPanel.tsx
@@ -22,15 +22,15 @@ const ProductPanel = () => {
 
   return (
     <main className="-mt-55 relative min-h-screen grid grid-cols-12 gap-4 p-10 max-md:p-5">
-      <div className=" z-10 col-span-12 mt-10 md:col-start-2 md:col-span-10 bg-default p-10 max-md:p-5  overflow-hidden rounded-2xl text-white space-y-10">
+      <div className=" z-10 col-span-12 mt-10 md:col-start-2 md:col-span-10 bg-default p-10 max-md:p-5  overflow-hidden rounded-2xl text-white space-y-5">
         <section className="flex justify-between items-center flex-col mt-7 ">
           <div className="relative w-[50%] max-md:w-full flex justify-center flex-col gap-3 items-center">
             <Image
               alt="arrow image"
               src={"/vector.svg"}
-              width={300}
-              height={300}
-              className="absolute z-0 bottom-2 left-80 max-md:hidden"
+              width={270}
+              height={270}
+              className="absolute z-0 bottom-2 left-58 max-md:hidden"
             />
             <Image
               alt="arrow image"
@@ -39,7 +39,7 @@ const ProductPanel = () => {
               height={250}
               className="absolute z-0 bottom-15 left-30 hidden max-md:block"
             />
-            <h1 className="z-10 text-3xl font-bold mb-2">Our Collection</h1>
+            <h1 className="z-10 text-3xl font-bold ">Our Collection</h1>
             <p className=" z-10 bg-text-color font-medium w-full max-md:w-full text-center">
               Introducing our Coffee Collection, a selection of unique coffees
               from different roast types and origins, expertly roasted in small
